2c260005587405c47957c62b31582400dad4000d
gabriel fernandes <gabriel.fernandes.1@etu.u-bordeaux.fr>
2026-05-26 02:43:18 +0200

    initial commit

---
+++ src/main/java/hallow/vessel/VesselDataGenerator.java
@@ -0,0 +1,11 @@
+package hallow.vessel;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VesselDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
